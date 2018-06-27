757a0f27677135133e685753b1ccfa683766361a
harry <ngocthaicao@gmail.com>
2018-06-27 15:45:55 +0700

    Add Todo Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_27_080929) do
+ActiveRecord::Schema.define(version: 2018_06_27_083046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2018_06_27_080929) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "todos", "users"
 end
