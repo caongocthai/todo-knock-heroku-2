fbc9aebefb06c3078a18ed6ac511dbc1404429c0
harry <ngocthaicao@gmail.com>
2018-06-27 15:14:35 +0700

    Add User Model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates_presence_of :name, :password_digest
+
+  validates :email,
+            presence: true,
+            length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.new
+admin.name = 'Harry Admin'
+admin.email = 'admin@harry.com'
+admin.password = 'harryAdmin'
+admin.password_confirmation = 'harryAdmin'
+admin.admin = true
+admin.save
+
+user = User.new
+user.name = 'Harry Bro'
+user.email = 'user@harry.com'
+user.password = 'harryBro'
+user.password_confirmation = 'harryBro'
+user.save
